390051447ef9b6d8f2309a2a2e0059fbfed88ede
devganesh <devganesh91@gmail.com>
2020-07-26 21:47:53 +0530

    Added prop_features model

--- Routes/property.js
+++ Routes/property.js
@@ -1,33 +1,63 @@
 const express = require("express");
 const router = express.Router();
 let jwt = require("jsonwebtoken");
-const { QueryTypes } = require("sequelize");
+const { QueryTypes, Op } = require("sequelize");
 const { body, validationResult } = require("express-validator");
 
 const db = require("../models");
 
 router.get("/", (req, res) => {
   let pageNum = req.query.pageNum;
+  let rating = req.query.rating;
+  let category = req.query.category;
 
-  console.log(pageNum);
+  console.log(pageNum, rating);
 
   if (pageNum != undefined && pageNum > 0) {
-    pageNum = (pageNum - 1) * 50;
+    pageNum = (pageNum - 1) * 20;
   } else {
     pageNum = 0;
+  }
+
+  if (rating === undefined) {
+    console.log("rating", rating);
+    rating = [1, 2, 3, 4, 5];
+  } else {
+    rating = rating.split(',');
+  }
+
+  if (category === undefined) {
+    console.log("category", category);
+    category = ['Apartment', 'House', 'Villa', 'Cottage']
+  } else {
+    category = category.split(',');
   }
 
   db.properties
     .findAll({
       attributes: { exclude: ["id", "createdAt", "updatedAt"] },
       offset: pageNum,
-      limit: 50,
+      limit: 20,
+      where: {
+        rating: rating,
+        category: category
+      }
     })
     .then((result) => res.send(result))
     .catch((err) => {
       console.log(err);
       res.status(500).json({ errormsg: "Internal Server Error" });
     });
+});
+
+
+router.get('/getTotalPageNum', async (req, res) => {
+  const totalPropertiesCount = await db.properties.count();
+
+  res.send({
+    TotalPageNum: totalPropertiesCount / 20,
+    totalPropertiesCount: totalPropertiesCount
+  })
 });
 
 // router.post('/add', (req, res) => {
